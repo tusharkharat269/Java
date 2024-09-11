ee26454fa4cc041768c892c699440b2c7aee1213
tusharkharat269 <tusharkharat003@gmail.com>
2024-09-11 12:13:52 +0530

    Added primitive data types

---
+++ Java/src/Basic_Concept/variables.java
@@ -0,0 +1,21 @@
+package Basic_Concept;
+
+public class variables {
+	public static void main(String[] args) {
+		String studentName = "ABC";
+
+		int studentAge = 23;
+		float studentFee = 75.25f;
+		char studentGrade = 'B';
+		boolean studentAdmintion = true;
+
+		// Print variables
+		System.out.println("Student name: " + studentName);
+		System.out.println("Student age: " + studentAge);
+		System.out.println("Student fee: " + studentFee);
+		System.out.println("Student grade: " + studentGrade);
+		System.out.println("student admition status: " + studentAdmintion);
+		
+	}
+
+}
